fix run_nerf_helpers.py; change gradient of light in run_ior.py

run_ior.py
@@ -349,7 +349,8 @@ class IoR_emissionAbsorptionODE(nn.Module):
         
         
         pts = y[:,0:3]
-        ray_dir = y[:,3:6]
+        # ray_dir = y[:,3:6]
+        omega = y[:,3:6]
         transmition_ = y[:,9:10]
         
         # query the 3D grid with trilinear interpolation
@@ -361,7 +362,7 @@ class IoR_emissionAbsorptionODE(nn.Module):
         # determining whether a point is inside the bounding box or not  
         # weights = get_bb_weights(pts,self.bounding_box_vals)
         outputs = self.voxel_grid_mlp(pts)
-        _, weights = torch.max(outputs.data, 1)
+        _, weights = torch.min(outputs.data, 1)
         weights = weights.reshape((-1, 1))
 
 
@@ -371,7 +372,7 @@ class IoR_emissionAbsorptionODE(nn.Module):
 
 
         # setting the density to zero for the points inside the bounding box
-        density= density*weights
+        # density= density*weights
 
         torch.cuda.empty_cache()
         
@@ -387,13 +388,21 @@ class IoR_emissionAbsorptionODE(nn.Module):
 
     
     
-        dv_ds = self.step_size*ior_grad
-        dx_ds = self.step_size*normalizing(ray_dir)
-        alpha = 1. - torch.exp(-density*self.step_size/self.n_samples)
-        alpha = alpha.clip(0.,1.)
-        dc_dt = transmition_*rgb*alpha*self.n_samples
-        dT_dt = -transmition_*alpha*self.n_samples
-        dy_dt = torch.cat([dx_ds,dv_ds,dc_dt,dT_dt],-1)
+        # dv_ds = self.step_size*ior_grad
+        # dx_ds = self.step_size*normalizing(ray_dir)
+        # alpha = 1. - torch.exp(-density*self.step_size/self.n_samples)
+        # alpha = alpha.clip(0.,1.)
+        # dc_dt = transmition_*rgb*alpha*self.n_samples
+        # dT_dt = -transmition_*alpha*self.n_samples
+        # dy_dt = torch.cat([dx_ds,dv_ds,dc_dt,dT_dt],-1)
+
+        domega_ds = ior_grad - omega * torch.einsum('ij, ij->i', omega, ior_grad).reshape(-1, 1)
+        dx_ds = omega
+        alpha = 1. - torch.exp(-density * self.step_size / self.n_samples)
+        alpha = alpha.clip(0., 1.)
+        dc_dt = transmition_ * rgb * alpha * self.n_samples
+        dT_dt = -transmition_ * alpha * self.n_samples
+        dy_dt = torch.cat([dx_ds, domega_ds, dc_dt, dT_dt], -1)
 
 
         return dy_dt